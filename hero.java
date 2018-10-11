631062995e9a3fc464025412d65f705281b5d4e2
wangxuanqi <43697755+wangxuanqi@users.noreply.github.com>
2018-10-11 17:41:34 +0800

    Add files via upload

---
+++ hero.java
@@ -0,0 +1,44 @@
+package wangzherongyao;
+
+public class hero {
+	public int HP;
+	public int MP;
+	public int EXP;
+	public int attack;
+	public hero()
+	{
+		
+	}
+	public hero(int h,int m,int e,int a)
+	{
+		HP = h;
+		MP = m;
+		EXP = e;
+		attack = a;
+	}
+	public int firstSkill()
+	{
+		return 0;
+	}
+	public int secondSkill()
+	{
+		return 0;
+	}
+
+	public int thirdSkill()
+	{
+		return 0;
+	}
+	public void attack()
+	{
+		
+	}
+	public int getAttack()
+	{
+		return attack;
+	}
+	public void print()
+	{
+		
+	}
+}
